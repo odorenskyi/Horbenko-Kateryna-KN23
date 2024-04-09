2e1d14c1cc9423a09e53e0f8e6e232faa04626d4
Kate Horbenko <adfrw445577@gmail.com>
2024-04-09 23:40:22 +0300

    Added function prototypes for task 9.1-9.3

--- lab08/prj/mod/ModulesHorbenko.h
+++ lab08/prj/mod/ModulesHorbenko.h
@@ -2,5 +2,8 @@
 #define MODULESHORBENKO_H_INCLUDED
 
 float s_calculation (float , float , float );
+std::string tornado_category(int);
+float* calculate_temp(float, float, float, float, float, float);
+unsigned int Bits(unsigned int num);
 
 #endif // MODULESHORBENKO_H_INCLUDED
